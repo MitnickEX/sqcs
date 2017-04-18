c6238219aae6bf435fde86d6dc2bf777efa31175
fudax <yijiubasi@163.com>
2017-04-18 15:23:27 +0800

    first commit

--- src/main/java/com/fudax/sqcs/version/service/MyWorkBenchService.java
+++
@@ -1,8 +0,0 @@
-package com.fudax.sqcs.version.service;
-
-import java.util.List;
-import java.util.LinkedHashMap;
-
-public interface MyWorkBenchService {
-	public List<LinkedHashMap<String, String>> getWorkdata(LinkedHashMap<String, String> dataMap);
-}
--- src/main/java/com/fudax/sqcs/version/service/ReleaseAnalysisService.java
+++
@@ -1,9 +0,0 @@
-package com.fudax.sqcs.version.service;
-
-import java.util.LinkedHashMap;
-
-import com.alibaba.fastjson.JSONObject;
-
-public interface ReleaseAnalysisService {
-	public JSONObject getPlusCharts(LinkedHashMap<String, String> dataMap);
-}
